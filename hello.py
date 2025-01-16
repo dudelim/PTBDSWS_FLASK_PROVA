7c2a9635dc9e4f1dc3ec91fc2603cab404c1ca43
Your Name <you@example.com>
2025-01-16 23:26:18 +0000

    [AVALIAÇÃO]

--- hello.py
+++ hello.py
@@ -1,11 +1,111 @@
+import os
+from flask import Flask, render_template, session, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
 
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class NameForm(FlaskForm):
+    name = StringField('Cadastre o novo professor:', validators=[DataRequired()])
+    role = SelectField('Disciplina associada:', choices=[('DSWA5'), ('GPSA5'), ('IHCA5'), ('SODA5'), ('PJIA5'), ('TCOA5')])
+    submit = SubmitField('Cadastrar')
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
+@app.context_processor
+def inject_current_time():
+    return {'current_time': datetime.utcnow()}
+
+
+@app.route('/<option>')
+def unavailable(option):
+    title_map = {
+        'disciplinas': 'Cadastro de Disciplinas',
+        'alunos': 'Cadastro de Alunos',
+        'cursos': 'Cadastro de Cursos',
+        'ocorrencias': 'Cadastro de Ocorrências'
+    }
+    title = title_map.get(option, 'Não disponível')
+    return render_template('unavailable.html', title=title)
+
+
+
+@app.route('/', methods=['GET'])
+def index():
+    aluno_nome = "Maria Eduarda Melim de Oliveira"
+    prontuario = "PT3025594"
+    data_hora_local = datetime.now().strftime("%B %d, %Y %I:%M %p")
+    return render_template(
+        'index.html',
+        aluno_nome=aluno_nome,
+        prontuario=prontuario,
+        data_hora_local=data_hora_local
+    )
+
+
+@app.route('/professores', methods=['GET', 'POST'])
+def professores():
+    form = NameForm()
+    user_all = User.query.all()
+    role_all = Role.query.all()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user_role = Role.query.filter_by(name=form.role.data).first()
+            user = User(username=form.name.data, role=user_role)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('professores'))
+
+    return render_template(
+        'professores.html',
+        form=form,
+        user_all=user_all,
+        role_all=role_all
+    )
